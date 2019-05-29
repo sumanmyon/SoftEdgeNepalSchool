39341fcdd47e129a1fb37c02eda21866210f76e5
Suman Neupane <sumanmyon.sam@gmail.com>
2019-05-29 11:17:09 +0545

    Setting required gradle files, app permissions and other resource files for project.

--- app/src/main/java/www/softedgenepal/com/softedgenepalschool/MainActivity.java
+++ app/src/main/java/www/softedgenepal/com/softedgenepalschool/MainActivity.java
@@ -1,7 +1,13 @@
 package www.softedgenepal.com.softedgenepalschool;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.FutureTarget;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
